a217980510b40c39d46f52eecba44967b42372f1
Jacob <jacob.hanafin@vantagepnt.com>
2021-02-26 13:55:37 -0600

    Update for python3 and some code tweaks.

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 Based on: https://github.com/jpmenil/zabbix-templates/isc-dhcp
 
-# on dhcp server
-* install dependencies:
-    * pip install pypureomapi
-    * pip install netaddr
-* mv userparameter_dhcp.conf /etc/zabbix/zabbix_agentd.d/
-* mv check_dhcp_leases.py /usr/local/bin/
-* Add lines like #= NAME =# to the end of all range statements, if you want.
-* edit check_dhcp_leases.py to fix your omapi key and key name, also, probably the location of your dhcpd.conf file.
+# On the DHCP Server
+1. Install dependencies:  
+  1a. pip3 install -r ./requirements.txt
+2. Move ```userparameter_dhcp.conf``` to ```/etc/zabbix/zabbix_agentd.d/```
+3. Move ```check_dhcp_leases.py``` to ```/usr/local/bin/```
+4. Add lines like ```#= NAME =#``` to the end of all range statements, if you want.
+5. Edit check_dhcp_leases.py to fix your omapi key and key name, also, probably the location of your dhcpd.conf file.
 
-# import templates in zabbix
+# Generate an OMAPI key for dhcp (if you have not already done so)
+1. /usr/sbin/dnssec-keygen -a HMAC-MD5 -b 512 -n HOST defomapi
+2. cat ./Kdefomapi*
+3. Copy your key
+4. Add the omapi config to your dhcp config
+Example:
+```
+omapi-port 7911;
+omapi-key defomapi;
+key defomapi {
+     algorithm hmac-md5;
+     secret *paste your key here*;
+}
+```
+
+# Import templates in zabbix
 * add graphs
-
-As far as i know, there is no native way implemented by ISC dhcp to request free lease.
-
-Two choices, parse the dhcpd.leases file, or do it via omapi.
-
-The script is using omapi and is far from perfect.
-
-# Hacks by myself from original
-* Deal with missing names of range statements
-* Change template/code to pull all the values so you can make a stacked graph
-* slightly hack up the template
-* Zabbix 4.0
--- check_dhcp_leases.py
+++ check_dhcp_leases.py
@@ -1,6 +1,7 @@
-#!/usr/pkg/bin/python2.7
+#!/bin/python3
 
 from netaddr import iter_iprange
+import argparse
 import itertools
 import json
 import os
@@ -8,56 +9,70 @@ import pypureomapi
 import re
 import sys
 
-KEYNAME = 'omapi_key'
-BASE64_ENCODED_KEY = 'g8pw3Kc7NfbXMEaMg=='
-dhcp_server_ip = '127.0.0.1'
-port = 7911 # Port of the omapi service
-OMAPI_OP_UPDATE = 3
 DHCP_CONF = '/usr/pkg/etc/dhcp/dhcpd.conf' if os.path.isfile('/usr/pkg/etc/dhcp/dhcpd.conf') else '/etc/dhcp/dhcpd.conf'
+leases_states = {
+    1: 'free',
+    2: 'active',
+    3: 'expired',
+    4: 'released',
+    5: 'abandoned',
+    6: 'reset',
+    7: 'backup',
+    8: 'reserved',
+    9: 'bootp'
+}
 
-def discoverRange():
+class OMAPI:
+    KEYNAME = 'defomapi'
+    BASE64_ENCODED_KEY = '6h54k4gpHqzTLtNo2ZKZUKLWc5ugjMGXUkdYe05I3qMdESyRhuIu13wKDHcgVY3D+kwYUIlR0AZRYmDRso3hqg=='
+    ip = '127.0.0.1'
+    port = 7911
+    OMAPI_OP_UPDATE = 3
+    def __init__(self):
+        self.conn = pypureomapi.Omapi(
+            self.ip,
+            self.port,
+            self.KEYNAME.encode('utf_8'),
+            self.BASE64_ENCODED_KEY.encode('utf_8')
+        )
+    def get_lease_state(self, ip):
+        msg = pypureomapi.OmapiMessage.open(b"lease")
+        msg.obj.append((b"ip-address", pypureomapi.pack_ip(str(ip))))
+        response = self.conn.query_server(msg)
+        if response.opcode != self.OMAPI_OP_UPDATE:
+            print('ZBX_NOTSUPPORTED')
+        else:
+            return ord(response.obj[0][1][-1:])
 
-    reg_range = re.compile('range\s(.*?);')
-    reg_name = re.compile('#= (.*?) =#')
-    ranges = []
-    with open(DHCP_CONF, 'r') as f:
-        for key, group in itertools.groupby(f, lambda line: line.startswith('\n')):
-            if not key:
-                subnet_info = list(group)
-                name = [m.group(1) for l in subnet_info for m in [reg_name.search(l)] if m]
-                range_list = [m.group(1) for l in subnet_info for m in [reg_range.search(l)] if m]
-                if range_list:
-                    for num, range in enumerate(range_list):
-                        ip_start = range.split(' ')[0]
-                        ip_end = range.split(' ')[1]
-                        ips = list(iter_iprange(ip_start, ip_end, step=1))
-                        myname = 'IP Range {0}'.format(str(num))
-                        if num < len(name):
-                            myname = name[num]
-                        ranges.append({'{#NAME}': myname, '{#RANGE}': '{0}-{1}'.format(ip_start, ip_end), '{#TOTAL}': len(ips)})
+class Ranges:
+    def discover():
+        reg_range = re.compile('range\s(.*?);')
+        reg_name = re.compile('#= (.*?) =#')
+        ranges = []
+        with open(DHCP_CONF, 'r') as f:
+            for key, group in itertools.groupby(f, lambda line: line.startswith('\n')):
+                if not key:
+                    subnet_info = list(group)
+                    name = [m.group(1) for l in subnet_info for m in [reg_name.search(l)] if m]
+                    range_list = [m.group(1) for l in subnet_info for m in [reg_range.search(l)] if m]
+                    if range_list:
+                        for num, range in enumerate(range_list):
+                            ip_start = range.split(' ')[0]
+                            ip_end = range.split(' ')[1]
+                            ips = list(iter_iprange(ip_start, ip_end, step=1))
+                            myname = 'IP Range {0}'.format(str(num))
+                            if num < len(name):
+                                myname = name[num]
+                            ranges.append({'{#NAME}': myname, '{#RANGE}': '{0}-{1}'.format(ip_start, ip_end), '{#TOTAL}': len(ips)})
+        ranges_dict = {}
+        ranges_dict['data'] = ranges
+        return ranges_dict
 
-    ranges_dict = {}
-    ranges_dict['data'] = ranges
-    return json.dumps(ranges_dict)
-
-def checkRange(ipsList, ip_type):
-
-    ip_start = ipsList.split('-')[0]
-    ip_end = ipsList.split('-')[1]
-    ips = list(iter_iprange(ip_start, ip_end, step=1))
-    # man 8 dhcpd.conf
-    leases_states = {
-            1: 'free',
-            2: 'active',
-            3: 'expired',
-            4: 'released',
-            5: 'abandoned',
-            6: 'reset',
-            7: 'backup',
-            8: 'reserved',
-            9: 'bootp'
-            }
-    results = {
+    def check(ipsList, ip_type):
+        ip_start = ipsList.split('-')[0]
+        ip_end = ipsList.split('-')[1]
+        ips = list(iter_iprange(ip_start, ip_end, step=1))
+        results = {
             'Total': len(ips),
             'free': 0,
             'active': 0,
@@ -68,27 +83,67 @@ def checkRange(ipsList, ip_type):
             'reserved': 0,
             'bootp': 0,
             'released': 0
-            }
+        }
+        omapi= OMAPI()
+        for ip in ips:
+            state = omapi.get_lease_state(ip)
+            try:
+                results[leases_states[state]] += 1
+            except KeyError:
+                print ('ZBX_NOTSUPPORTED')
+        return (results[ip_type])
 
-    o = pypureomapi.Omapi(dhcp_server_ip, port, KEYNAME, BASE64_ENCODED_KEY)
-    for ip in ips:
-        msg = pypureomapi.OmapiMessage.open('lease')
-        msg.obj.append(('ip-address', pypureomapi.pack_ip(str(ip))))
-        response = o.query_server(msg)
-        if response.opcode != OMAPI_OP_UPDATE:
-            print 'ZBX_NOTSUPPORTED'
-        # Need to dig for the key error thingy
-        try:
-            state = ord(response.obj[0][1][-1:])
-            results[leases_states[state]] += 1
-        except KeyError:
-            print 'ZBX_NOTSUPPORTED'
+parser = argparse.ArgumentParser(
+    prog="Zabbix Lease Reporting",
+    description="""
+    A simple reporting script for Zabbix to enable DHCP collection.
+    """,
+    epilog="""
+    Please remember to edit the omapi key and key name in the OMAPI class.
+    """,
+)
+parser.add_argument(
+    "-d",
+    "--discover",
+    help="The initial discovery script for zabbix to return the IP ranges, names, and total number of IPs.",
+    action="store_true"
+)
+parser.add_argument(
+    "-c",
+    "--check",
+    help="Check and IP range (as reported by the --discover option)",
+    action="store_true"
+)
+parser.add_argument(
+    "--range",
+    help="The range to check (written as: 192.168.1.2-192.168.1.254)",
+    type=str,
+    default=None
+)
+parser.add_argument(
+    "--state",
+    help="Get the number of leases in the specified range that are in this state.",
+    choices=[
+        "free",
+        "active",
+        "expired",
+        "released",
+        "abandoned",
+        "reset",
+        "backup",
+        "reserved",
+        "bootp"
+    ],
+    type=str,
+    default=None
+)
+args = parser.parse_args()
 
-    print(results[ip_type])
-
-if __name__ == "__main__":
-
-    if len(sys.argv) < 2:
-        sys.exit(discoverRange()) 
-    else:
-        sys.exit(checkRange(sys.argv[1], sys.argv[2]))
+if args.discover:
+    ranges = Ranges.discover()
+    print(json.dumps(ranges))
+elif args.check:
+    num_in_state = Ranges.check(args.range, args.state)
+    print(num_in_state)
+else:
+    print("No option selected")
